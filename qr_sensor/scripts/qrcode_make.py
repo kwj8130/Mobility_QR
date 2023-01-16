b897bf24064cf614b5cea0f92e0d874adea2d1ee
kwj8130 <114387230+kwj8130@users.noreply.github.com>
2023-01-16 09:36:33 +0900

    Add files via upload

---
+++ qr_sensor/scripts/qrcode_make.py
@@ -0,0 +1,26 @@
+# pip3 install qrcode
+import qrcode
+
+img1 = qrcode.make("place1")
+img1.save("QR1")
+
+img2 = qrcode.make("place2")
+img2.save("QR2")
+
+img3 = qrcode.make("place3")
+img3.save("QR3")
+
+img4 = qrcode.make("place4")
+img4.save("QR4")
+
+img5 = qrcode.make("place5")
+img5.save("QR5")
+
+img6 = qrcode.make("place6")
+img6.save("QR6")
+
+img7 = qrcode.make("place7")
+img7.save("QR7")
+
+img8 = qrcode.make("place8")
+img8.save("QR8")
